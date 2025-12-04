e520020e1a28960a6716d43620e70c77c745ac48
georgeeduardtp <georgeeduard.tp@gmail.com>
2025-12-04 11:05:46 +0100

    he pasado el model de java a kotlin que me habia equivocado

---
+++ app/src/main/java/es/fpsumma/dam2/utilidades/model/Asignatura.kt
@@ -0,0 +1,22 @@
+package es.fpsumma.dam2.utilidades.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "asignaturas")
+data class Asignatura(
+    @PrimaryKey(autoGenerate = true)
+     val id: Int = 0,
+
+    @ColumnInfo(name = "alumno")
+     val alumno: String? = null,
+
+    @ColumnInfo(name = "asignatura")
+     val asignatura: String? = null,
+
+    @ColumnInfo(name = "trimestre")
+     val trimestre: String? = null,
+
+    @ColumnInfo(name = "nota")
+     val nota: Float? = null)
